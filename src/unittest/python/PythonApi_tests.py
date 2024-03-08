a70bdcd40275c36462ece8c9162219b9aec9de7e
svishnud <vishnudurai.s@carrier.com>
2024-03-08 19:54:33 +0530

    Added python project setup

---
+++ src/unittest/python/PythonApi_tests.py
@@ -0,0 +1,12 @@
+from mockito import mock, verify
+import unittest
+
+from PythonApi import helloworld
+
+class PythonApiTest(unittest.TestCase):
+    def test_should_issue_hello_world_message(self):
+        out = mock()
+
+        helloworld(out)
+
+        verify(out).write("Hello world of Python\n")
